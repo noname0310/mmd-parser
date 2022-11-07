ffc004f0c3d25bcf6879b1173a821865a0dce157
noname <48761044+noname0310@users.noreply.github.com>
2022-11-07 15:53:33 +0900

    change export member

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import { CharsetEncoder } from "./CharsetEncoder";
 import { Parser } from "./Parser";
 
-const MMDParser = {
-    CharsetEncoder: CharsetEncoder,
-    Parser: Parser
-};
-
-export { CharsetEncoder, MMDParser, Parser };
+export { CharsetEncoder, Parser as MMDParser };
