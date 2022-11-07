8424b4a046d0eb772aecb4398bca1d8b4c3e9e23
noname <48761044+noname0310@users.noreply.github.com>
2022-11-07 15:53:08 +0900

    convert DataCreationHelper to typescript

--- src/DataCreationHelper.ts
+++ src/DataCreationHelper.ts
@@ -1,61 +1,38 @@
-/**
- * @author takahiro / https://github.com/takahirox
- */
+import type { Quaternion, Vector3 } from "./Math";
 
-function DataCreationHelper () {
-}
-
-DataCreationHelper.prototype = {
-
-    constructor: DataCreationHelper,
-
-    leftToRightVector3: function ( v ) {
-
+export class DataCreationHelper {
+    public static leftToRightVector3(v: Vector3): void {
         v[ 2 ] = -v[ 2 ];
-
-    },
-
-    leftToRightQuaternion: function ( q ) {
-
+    }
+    
+    public static leftToRightQuaternion(q: Quaternion): void {
         q[ 0 ] = -q[ 0 ];
         q[ 1 ] = -q[ 1 ];
+    }
 
-    },
-
-    leftToRightEuler: function ( r ) {
-
+    public static leftToRightEuler(r: Vector3): void {
         r[ 0 ] = -r[ 0 ];
         r[ 1 ] = -r[ 1 ];
+    }
 
-    },
-
-    leftToRightIndexOrder: function ( p ) {
-
+    public static leftToRightIndexOrder(p: Vector3): void {
         const tmp = p[ 2 ];
         p[ 2 ] = p[ 0 ];
         p[ 0 ] = tmp;
+    }
 
-    },
-
-    leftToRightVector3Range: function ( v1, v2 ) {
-
+    public static leftToRightVector3Range(v1: Vector3, v2: Vector3): void {
         const tmp = -v2[ 2 ];
         v2[ 2 ] = -v1[ 2 ];
         v1[ 2 ] = tmp;
+    }
 
-    },
-
-    leftToRightEulerRange: function ( r1, r2 ) {
-
+    public static leftToRightEulerRange(r1: Vector3, r2: Vector3): void {
         const tmp1 = -r2[ 0 ];
         const tmp2 = -r2[ 1 ];
         r2[ 0 ] = -r1[ 0 ];
         r2[ 1 ] = -r1[ 1 ];
         r1[ 0 ] = tmp1;
         r1[ 1 ] = tmp2;
-
     }
-
-};
-
-export { DataCreationHelper };
+}
---
+++ src/Math.ts
@@ -0,0 +1,2 @@
+export type Vector3 = [ number, number, number ];
+export type Quaternion = [ number, number, number, number ];
