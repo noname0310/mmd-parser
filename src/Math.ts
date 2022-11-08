6922f4716e801e78c8891335c5b330f38d33e9b2
noname <48761044+noname0310@users.noreply.github.com>
2022-11-08 15:38:38 +0900

    add vector2

--- src/Math.ts
+++ src/Math.ts
@@ -1,2 +1,3 @@
-export type Vector3 = [ number, number, number ];
-export type Quaternion = [ number, number, number, number ];
+export type Vector2 = [number, number];
+export type Vector3 = [number, number, number];
+export type Quaternion = [number, number, number, number];
